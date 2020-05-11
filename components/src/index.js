8e537534aa9851209187afc3c9908d1ba53c1cea
ebbitten <adam.helitzer@gmail.com>
2020-05-11 17:15:21 -0500

    section 6

--- components/src/index.js
+++ components/src/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
+
 
 const App = () => {
-    return <div> Hi there!</div>
+    return (
+        <div className="ui container comments">
+            <ApprovalCard>
+                <div>
+                <h4>Warning!</h4>
+                Are you sure you want to do this?
+                </div>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Sam" timeAgo="Today at 4:45PM" commentText="Great Article!" avatar={faker.image.avatar()} />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail author="Alex" timeAgo="Today at 2:00AM" commentText="Really informative" avatar={faker.image.avatar()} />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" commentText="I disagree" avatar={faker.image.avatar()} />
+            </ApprovalCard>
+        </div>
+    );
 };
 
 ReactDom.render(<App />, document.querySelector('#root'))
